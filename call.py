23180c06cc3a7458ba72854062cabe8a4f7b82b4
epicgk11 <epicgeek11@gmail.com>
2025-03-14 22:47:00 +0530

    edited requirements added button to go to existing trees. also removed link to button

--- call.py
+++ call.py
@@ -6,8 +6,15 @@ from custom2doctreen_parser import CustomToDoctreenConverter
 def main():
     doctreen_icon = "https://static.wixstatic.com/media/cb6226_4224827f5f13449ebb1ce7b71abbbc10%7Emv2.png/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/cb6226_4224827f5f13449ebb1ce7b71abbbc10%7Emv2.png"
     doctreen_logo = "https://static.wixstatic.com/media/cb6226_9226c5ad3a1a48e9abb5adbf8e8eb30a~mv2.png/v1/crop/x_53,y_0,w_1223,h_439/fill/w_291,h_104,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Logo%20horizontal%20fond%20blanc.png"
+    
     st.set_page_config(page_title="Medical Tree Generator", page_icon=doctreen_icon)
-
+    tree_tuples = (None,'Email', 'Home phone', 'Mobile phone')
+    option = st.selectbox('Existing AI Generated Trees',tree_tuples)
+    
+    if (option):
+        alreadyLink = f"https://front.interns.doctreen.io/edit/{option}"
+        st.link_button("Click Here to go selected tree",alreadyLink)
+    
     st.markdown(f"""
         <div style="text-align: center;">
             <img src="{doctreen_logo}" width="200">
@@ -39,14 +46,11 @@ def main():
         try:
             converter = CustomToDoctreenConverter(owner_id, tree_name)
             doctreen_nodes, _, link = converter.convert_custom_to_doctreen(tree)
-            # with open('Doctreen_Combined_Tree.json', "w", encoding="utf-8") as outfile:
-            #     json_str = json_util.dumps(doctreen_tree, indent=4, ensure_ascii=False)
-            #     outfile.write(json_str)
             
             st.success("Conversion complete!")
             st.write(f"Total nodes inserted: {len(doctreen_nodes)}")
             st.warning(f"Please Log onto doctreen to view the tree",icon="⚠️")
-            st.markdown(f"[Link to tree]({link})")
+            st.link_button("Click here to go to the generated tree",link)
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
